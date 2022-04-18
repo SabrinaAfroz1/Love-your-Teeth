29f788360d2636b245e0209060dd8296eee98620
SabrinaAfroz1 <sabrina1234091@gmail.com>
2022-04-19 01:49:40 +0600

    about complete

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h2>About section </h2>
+        <div className="p-5 m-5 border rounded">
+
+            <p className="fs-3 text-dark ">
+                As I am student of CSE background, I wanted to be a software Engineer. But I like web development most and want to be a web developer . Then I decided that I have to learn more and more to develop my skill . As I am doing this course to achieve my goal , I try to be focused all the time . My next target is after completing this course, I try to do some project-based work with my friend as team.
+            </p>
         </div>
     );
 };
---
+++ src/Pages/Shared/404page/Errorpage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Errorpage = () => {
+    return (
+        <div className="bg-dark">
+            <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2F404-page&psig=AOvVaw0rzZY6Loq6ldOjt-aMtMf9&ust=1650397685232000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCJDY3NKwnvcCFQAAAAAdAAAAABAD" alt="" />
+
+        </div>
+    );
+};
+
+export default Errorpage;
