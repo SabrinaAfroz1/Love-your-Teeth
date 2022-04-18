8fb098d1d9e3be4c00ae2f1c2afc92158aae28a2
SabrinaAfroz1 <sabrina1234091@gmail.com>
2022-04-18 21:03:39 +0600

    section done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import About from './Pages/About/About';
 import Blog from './Pages/Blog/Blog';
 import Home from './Pages/Home/Home';
+import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
 
@@ -14,6 +16,8 @@ function App() {
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
         <Route path='/about' element={<About></About>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='register' element={<Register></Register>}></Route>
         <Route path='/blog' element={<Blog></Blog>}></Route>
       </Routes>
       <Footer></Footer>
--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import teeth_img from '../../../header_img.jpg';
+import section_img from '../../../section_img.jpg';
 
 const Header = () => {
     return (
@@ -30,8 +31,18 @@ const Header = () => {
                 </Container>
             </Navbar>
 
-            <div>
-
+            <div className="container mt-5">
+                <div class="row">
+                    <div class="col-sm-12 col-lg-8 mt-5">
+                        <h2 className="fs-1">Dental Implants Specialist</h2>
+                        <p className="fs-5 mt-4">At The Center for High Tech Dentistry, the goal of Simon W. Rosenberg, DMD, and the team is to provide comprehensive oral health care to people of all ages living in and around the Upper East Side of Manhattan and throughout New York City.</p>
+                        <button type="button" class="btn btn-dark px-4 fs-4 mt-5">Learn More</button>
+                    </div>
+                    <div class="col-sm-6 col-lg-4">
+                        <img className=" rounded-circle" width="500"
+                            height="500" src={section_img} alt="" srcset="" />
+                    </div>
+                </div>
             </div>
 
         </div>
