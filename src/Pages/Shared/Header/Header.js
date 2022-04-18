9fd2d1b0a943617b85bf241a9673921c0a579f34
SabrinaAfroz1 <sabrina1234091@gmail.com>
2022-04-18 20:46:55 +0600

    header section done, routing and component ready

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import teeth_img from '../../../header_img.jpg';
 
 const Header = () => {
     return (
         <div>
-            <h2> This is header </h2>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand as={Link} to='/'>
+                        <img
+                            alt=""
+                            src={teeth_img}
+                            width="40"
+                            height="40"
+                            className="d-inline-block align-top rounded-circle "
+                        />{' '}
+                        <span className="fw-bolder fs-3">Love your Teeth</span>
+                    </Navbar.Brand>
+
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse className="justify-content-end" id="responsive-navbar-nav">
+                        <Nav >
+                            <Nav.Link as={Link} to='/login'>Login</Nav.Link>
+                            <Nav.Link as={Link} to='/about'>About Us</Nav.Link>
+                            <Nav.Link as={Link} to='/blog'>Blogs</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <div>
+
+            </div>
+
         </div>
     );
 };
